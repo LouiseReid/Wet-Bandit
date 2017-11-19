84e61f1f8c4b38cf8e4c452f9a954710c27c54e3
LouiseReid <l_reid@hotmail.co.uk>
2017-11-19 21:34:06 +0000

    sound effects and some stylying

--- app/src/main/java/com/example/louisereid/wetbandit/MainActivity.java
+++ app/src/main/java/com/example/louisereid/wetbandit/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.louisereid.wetbandit;
 
+import android.media.Image;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView credit;
     private TextView jackpot;
     private Button collect;
-    private Button topup5;
-    private Button topup10;
-    private Button topup20;
+    private ImageButton topup5;
+    private ImageButton topup10;
+    private ImageButton topup20;
     private ImageButton spin;
     private Machine machine;
     private ArrayList<Symbol> result;
@@ -39,9 +41,9 @@ public class MainActivity extends AppCompatActivity {
         credit = (TextView)findViewById(R.id.creditView);
         jackpot = (TextView)findViewById(R.id.jackpotView);
         collect = (Button)findViewById(R.id.collectBtn);
-        topup5 = (Button)findViewById(R.id.tu5);
-        topup10 = (Button)findViewById(R.id.tu10);
-        topup20 = (Button)findViewById(R.id.tu20);
+        topup5 = (ImageButton)findViewById(R.id.tu5);
+        topup10 = (ImageButton)findViewById(R.id.tu10);
+        topup20 = (ImageButton)findViewById(R.id.tu20);
         spin = (ImageButton)findViewById(R.id.spinBtn);
 
         credit.setText(machine.getCredit().toString());
@@ -65,27 +67,29 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void ontopUpBtn5Press(View button){
-        machine.topUp5();
+    public void ontopUpBtnPress(View button){
+        if (button == topup5){
+            machine.topUp5();
+        }
+        else if (button == topup10){
+            machine.topUp10();
+        }
+        else if(button == topup20){
+            machine.topUp20();
+        }
         credit.setText(machine.getCredit().toString());
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.kaching);
+        mp.start();
     }
 
-    public void ontopUpBtn10Press(View button){
-        machine.topUp10();
-        credit.setText(machine.getCredit().toString());
-
-    }
-
-    public void ontopUpBtn20Press(View button){
-        machine.topUp20();
-        credit.setText(machine.getCredit().toString());
-    }
 
     public void onCollectBtnPress(View button){
         machine.collect();
         machine.setBank(1000);
         credit.setText(machine.getCredit().toString());
         jackpot.setText(machine.getBank().toString());
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.keepthechange);
+        mp.start();
 
     }
 }
